76bb9c562416177b1a308191bd4f3d268260b970
praveen-kumar-007 <praveen.pr105@gmail.com>
2026-08-06 15:04:23 +0530

    fix: add public frontend production vercel URL to CORS whitelist

--- backend/server.js
+++ backend/server.js
@@ -75,6 +75,7 @@ app.use(
       "http://localhost:5174",
       "http://localhost:5175",
       "https://multimarg.vercel.app",
+      "https://publicmultimarg.vercel.app",
       "https://multimargcarriers.co.in",
       "https://www.multimargcarriers.co.in",
       process.env.FRONTEND_ORIGIN,
